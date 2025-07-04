4bc4d059bf3c0d28f8405c0d9bace4a62057d15b
Milanpatel35 <patelmilan3435@gmail.com>
2025-07-04 16:37:17 +0530

    feat: Add initial implementation for test_doc including JSON, JS, and Python files

---
+++ sjc/sjc/doctype/test_doc/test_test_doc.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, samarthtechnoverse and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testtest_doc(FrappeTestCase):
+	pass
